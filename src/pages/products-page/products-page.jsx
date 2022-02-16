6100ae598d084dc0a5e473f6f2fb4b2e3afd736f
Alexander <sharodyu@gmail.com>
2022-02-16 15:31:25 +0300

    fix product-page

--- src/pages/products-page/products-page.jsx
+++ src/pages/products-page/products-page.jsx
@@ -2,7 +2,6 @@ import { CardsProducts } from "../../components/products-page/cards-products";
 import { FilterProducts } from "../../components/products-page/filter-products";
 import { HeaderProducts } from "../../components/products-page/header-products";
 
-let activeMenuItem = document.querySelector('#root > div > div.header > div.wrapper > div > div.menu > a:nth-child(2) > span');
 
 
 export const ProductsPage = ({ productType }) => {
